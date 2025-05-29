7c614fae76562e7f51e87af6ffd6ddb8a4c6bc2e
Paul R. Novak <paul.r.novak@gmail.com>
2025-05-28 21:46:07 -0500

    initial commit, adding files

---
+++ script.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+// display time as hh:mm
+const options = {
+    timeZone:"America/Chicago",
+    hour12 : true,
+    hour:  "2-digit",
+    minute: "2-digit",
+}
+
+var today = new Date();
+
+var time = today.toLocaleTimeString("en-US", options);
+
+document.getElementById("time").textContent = time;
+
